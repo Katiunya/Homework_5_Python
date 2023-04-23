f4bd44ad2865021c15d26943d03a46d776a2c213
Ekaterina Pabauskaya <Katiunya0103@gmail.com>
2023-04-23 16:32:24 +0300

    The tasks 26, 28 and 37 added

---
+++ task_37.py
@@ -0,0 +1,20 @@
+# Задача 37
+# Дано натуральное число N и
+# последовательность из N элементов.
+# Требуется вывести эту последовательность в
+# обратном порядке.
+# Примечание. В программе запрещается
+# объявлять массивы и использовать циклы
+# (даже для ввода и вывода).
+# Input: 2 -> 3 4
+# Output: 4 3
+
+n = int(input("Enter the number: "))
+
+def change(n):
+    if n == 0:
+        return ""
+    num = int(input("Enter the sequence numbers: "))
+    return change(n-1) + f'{num}'
+#    return f'{num}' + change(n-1)
+print(change(n))
